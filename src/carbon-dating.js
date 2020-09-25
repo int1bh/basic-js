71ac492b0c5376718a810c4e948dd3bfd6f5bc75
Alexandr <alexandrdryagin@yandex.ru>
2020-09-25 11:06:58 +0300

    carbon complete

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,16 +4,15 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
+  const k = 0.693 / HALF_LIFE_PERIOD;
   
+
+  if(typeof(sampleActivity) !== 'string' || parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) > 15 || isNaN(parseFloat(sampleActivity))){
+    return false;
+  } 
+
+ return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / k );
+
   
 };
 
-function dateSample(sampleActivity) {
-  if (typeof(sampleActivity) != 'string') {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-console.log(dateSample(1));
