16046803bd4f614d46276626cfa906dfa010860f
Alexandr <alexandrdryagin@yandex.ru>
2020-09-25 14:35:36 +0300

    dream-team complite

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  } else {
+  let filtered = members.filter(function(name) {
+    return typeof(name) === 'string';
+    
+  });
+  let sortArr = filtered.join(',').toUpperCase().split(',');
+console.log(sortArr);
+  let dream = sortArr.map(function(name) {
+    let trimName = name.trim();
+    return trimName.slice(0,1);
+  });
+  return dream.sort().join('');
+}
 };
